0b076943e9383a43a71fce2d0b9b3dba49b6c5ab
Stanisław Biegała <72930726+Bieslaw@users.noreply.github.com>
2023-03-10 08:32:12 +0100

    Update Class1.cs

--- ClassLibrary1/Class1.cs
+++ ClassLibrary1/Class1.cs
@@ -1,8 +1,30 @@
-﻿using System;
+using System;
 
 namespace ClassLibrary1
 {
-    public class Class1
+    public class StringCalculator
     {
+        public int Add(string numbers)
+        {
+            if(numbers.Length == 0)
+            {
+                return 0;
+            }
+            string[] splited = numbers.Split(',', '\n');
+            int sum = 0;
+            for (int i = 0; i < splited.Length; i++)
+            {
+                int number = Convert.ToInt32(splited[i]);
+                if(number < 0)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+                if(number <= 1000)
+                {
+                    sum += number;
+                }
+            }
+            return sum;
+        }
     }
 }
